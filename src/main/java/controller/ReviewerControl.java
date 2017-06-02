76d210e732890c473b9395b567ed7913c814de28
Dragos <dragosbad2009@gmail.com>
2017-06-02 17:27:27 +0300

    Reviewer Update

--- src/main/java/controller/ReviewerControl.java
+++ src/main/java/controller/ReviewerControl.java
@@ -1,5 +1,6 @@
 package controller;
 
+import com.sun.jmx.mbeanserver.Repository;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
--- src/main/java/services/ReviewerService.java
+++ src/main/java/services/ReviewerService.java
@@ -1,7 +1,11 @@
 package services;
 
+import controller.ReviewerControl;
+import repository.ReviewerRepository;
+
 /**
  * Created by Dragos on 5/8/2017.
  */
 public class ReviewerService {
+
 }
