6abdd5aa8537c6d44b23b609ac3625656f1ff196
Dragos <dragosbad2009@gmail.com>
2017-06-02 18:40:41 +0300

    Added participant Hibernation

---
+++ src/main/java/model/Participants.java
@@ -0,0 +1,54 @@
+package model;
+
+/**
+ * Created by Dragos on 6/2/2017.
+ */
+public class Participants
+{
+    private int idpa;
+    private String typepa;
+    private int ida;
+    private int idat;
+    private int ids;
+
+    public int getIdpa() {
+        return idpa;
+    }
+
+    public void setIdpa(int idpa) {
+        this.idpa = idpa;
+    }
+
+    public String getTypepa() {
+        return typepa;
+    }
+
+    public void setTypepa(String typepa) {
+        this.typepa = typepa;
+    }
+
+    public int getIda() {
+        return ida;
+    }
+
+    public void setIda(int ida) {
+        this.ida = ida;
+    }
+
+    public int getIdat() {
+        return idat;
+    }
+
+    public void setIdat(int idat) {
+        this.idat = idat;
+    }
+
+    public int getIds() {
+        return ids;
+    }
+
+    public void setIds(int ids) {
+        this.ids = ids;
+    }
+}
+
